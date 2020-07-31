20c762108c2cd1a8dc441f2f6d775954c597d219
Artem Vasylenko <artem.vasylenko@sigma.software>
2020-07-31 13:25:43 +0300

    initial commit

---
+++ AwesomeProject/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+import ScreenName1 from './screens/ScreenName1/ScreenName1'
+
+const App: () => React.Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
+          <View style={styles.body}>
+            {/* content */}
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  }
+});
+
+export default App;
---
+++ AwesomeProject/screens/ScreenName1/ScreenName1.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import { Text } from 'react-native';
+
+
+class ScreenName1 extends React.Component {
+
+    render() {
+        return (
+            <Text>Hello, I am ScreenName1! </Text>
+        );
+    }
+
+}
+
+export default ScreenName1;
